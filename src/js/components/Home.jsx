f6f36e74da93b4a87569fbc7299bbd26bb70a814
Antonio Borrero <antojoseabad@gmail.com>
2025-06-11 20:08:41 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [inputTodo, setInputTodo] = useState("");
+	const [list, setList] = useState([]);
+	const [count, setCount] = useState(0)
+	
+	function handleSubmit(event){
+		event.preventDefault();
+		if (inputTodo.trim() === ""){
+			return alert("No tasks, add a task");
+		};
+		setList(prev => [...prev, inputTodo]);
+		setInputTodo("");
+		setCount(prev => (prev += 1))
+	};
+
+	function deleteTodo(indexToDelete) {
+		setList(prev => prev.filter((_, index) => index !== indexToDelete));
+		setCount(prev => (prev -= 1));
+	}
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="title">To do's</h1>
+			<form onSubmit={handleSubmit}>
+				<input className="border border-top-0"
+				 type="text"
+				 onChange={(e) => setInputTodo(e.target.value)}
+				 value={inputTodo}
+				 placeholder="What needs to be done?"/>
+				<ul>
+					{list.map((inputTodo, index) => (
+						<li key={index}>{inputTodo}
+						<span className="delete" onClick={() => deleteTodo(index)}>x</span>
+						</li>
+					))}
+				</ul>
+				<p className="itemCount">{count} items left</p>
+				<button className="btn btn-primary">Add task</button>
+			</form>
 		</div>
 	);
 };
